02f414d88b30c73deaa0b3a8eddd40ada575857b
MauricioSts <Mauricio.santos.110@ufrn.edu.br>
2025-03-14 11:35:57 -0300

    Aprendendo sobre entrada de dados

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:app_entrada_de_dados/CampoTexto.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Campotexto(), debugShowCheckedModeBanner: false));
+}
